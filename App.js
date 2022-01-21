c0b441e08ba3a36f30bb8d4f617fb95e25969004
Hubert Kałuża <hkaluza@empik.com>
2022-01-21 22:05:51 +0100

    list with removable items

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem'
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'eat', key: '1'},
+    { text: 'drink', key: '2'},
+    { text: 'sleep', key: '3'}
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <View style={styles.content}>
+        {/*todo form*/}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={( {item} ) => (
+              // <Text>{item.text}</Text>
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -14,7 +41,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
